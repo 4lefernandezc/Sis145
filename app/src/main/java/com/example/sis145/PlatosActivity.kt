3b78fdc916f2dd0c28111840e50ef52ac638cf88
Alejandro Fernandez <luigiferca@gmail.com>
2023-05-23 20:15:27 -0400

    menu creado

--- app/src/main/java/com/example/sis145/PlatosActivity.kt
+++ app/src/main/java/com/example/sis145/PlatosActivity.kt
@@ -1,11 +1,22 @@
 package com.example.sis145
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class PlatosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_platos)
+
+        val BtnSopa = findViewById<ImageButton>(R.id.imgBtnSopa) as ImageButton
+
+        BtnSopa.setOnClickListener(){
+            val intent = Intent(this , SopaActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
