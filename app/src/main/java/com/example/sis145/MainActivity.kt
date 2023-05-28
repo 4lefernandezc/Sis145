49f13dd4675a6391df733af99d95ae89560db251
Alejandro Fernandez <luigiferca@gmail.com>
2023-05-27 20:20:53 -0400

    example login created

--- app/src/main/java/com/example/sis145/MainActivity.kt
+++ app/src/main/java/com/example/sis145/MainActivity.kt
@@ -10,32 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ButtonPlatos = findViewById<Button>(R.id.buttonPlatos) as Button
-        val ButtonBebidas = findViewById<Button>(R.id.buttonBebidas) as Button
-        val ButtonPostres = findViewById<Button>(R.id.buttonPostres) as Button
-        val ButtonSalir = findViewById<Button>(R.id.buttonSalir) as Button
-        val ButtonAcerca = findViewById<Button>(R.id.buttonAcerca) as Button
+        val ButtonInvitado = findViewById<Button>(R.id.buttonInvitado) as Button
 
-
-        ButtonPlatos.setOnClickListener(){
-            val intent = Intent(this , PlatosActivity::class.java)
-            startActivity(intent)
-        }
-        ButtonBebidas.setOnClickListener(){
-            val intent = Intent(this , BebidasActivity::class.java)
-            startActivity(intent)
-        }
-
-        ButtonPostres.setOnClickListener(){
-            val intent = Intent(this , PostresActivity::class.java)
-            startActivity(intent)
-        }
-        ButtonSalir.setOnClickListener(){
-            finish()
-        }
-
-        ButtonAcerca.setOnClickListener(){
-            val intent = Intent(this , AcercaActivity::class.java)
+        ButtonInvitado.setOnClickListener(){
+            val intent = Intent(this , MainMenuActivity::class.java)
             startActivity(intent)
         }
     }
